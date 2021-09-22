35262126c4467f45305e2fb4aee2c9c629faed4b
Ziad Reda <83479088+Wezza07@users.noreply.github.com>
2021-09-22 09:04:48 +0200

    Update 976. Largest Perimeter Triangle.cpp

--- 976. Largest Perimeter Triangle.cpp
+++ 976. Largest Perimeter Triangle.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
         int n = nums.size();
-        int res=0;
         sort(nums.rbegin() , nums.rend());
         for(int i=0 ; i<n-2 ; i++){
             int a = nums[i] , b=nums[i+1] , c=nums[i+2];
-            if(b+c>a){
-                res = a+b+c;
-                return res;
-            }
+            if(b+c>a) return a+b+c;
         }
         return 0;
     }
