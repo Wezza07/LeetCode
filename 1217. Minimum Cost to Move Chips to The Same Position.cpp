6488c7adf3586efc52bee6398d216e72d20355b1
Ziad Reda <83479088+Wezza07@users.noreply.github.com>
2021-09-22 16:15:43 +0200

    Create 1217. Minimum Cost to Move Chips to The Same Position.cpp

---
+++ 1217. Minimum Cost to Move Chips to The Same Position.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int even=0 , odd=0;
+        for(auto i:position){
+            if(i%2==0) even++;
+            else odd++;
+        }
+        return odd<=even ? odd : even;
+    }
+};
